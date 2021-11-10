e175cc8bcba09cf746c925d48dff156599a1028b
Marvin Largaespada <mlargaespada98.gonzalez@gmail.com>
2021-11-10 11:03:34 -0600

    Flack-Web50

---
+++ application.py
@@ -0,0 +1,103 @@
+from logging import debug
+import os
+from re import I
+
+from flask import Flask, redirect, request, session,sessions, url_for
+from flask.templating import render_template
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from time import localtime, strftime
+from collections import deque
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") or 'secreto'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+Channels = ['General']
+lista_mensaje = dict()
+lista_mensaje['General'] = deque(maxlen=100)
+
+@app.route("/")
+def index():
+    if request.method == 'GET':
+        # Validar al usuario si tiene una session abierta
+        if ('Username' in session):
+            return render_template("index.html", Channels = Channels)
+        else:
+            return render_template('formulario.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    session.clear()
+    # Inicio de session del usuario
+    if request.method == 'POST':
+        
+        username = request.form["Username"].strip()
+        #if ('Username' in session):
+         #   return redirect('/login')
+        #Users.append(username)
+        session['Username'] = username
+        return redirect("/")
+
+    return render_template('formulario.html')
+
+@app.route("/logout")
+def logout():
+    # Limpia session y retornamos a la pagina login
+    session.clear()
+    return redirect("/login")
+
+@socketio.on('message')
+def handleMessage(msg):
+    global lista_mensaje
+    datos = {'msgs': msg['mensaje'], 'user': msg['usuario'], 'tiempo': strftime('%I:%M',localtime())}
+    channel = msg['canal']
+
+    #Enviamos mensaje con socket
+    print('PRUEBA DE CANALES: ', msg['canal'])
+    if lista_mensaje.get(channel) is None:
+        lista_mensaje[channel] = deque(maxlen=100)
+    lista_mensaje[channel].append(datos)
+    print("message", lista_mensaje[channel], channel)
+    # Enviar Datos
+    emit('messages', datos, room=channel)
+
+# Creacion de nuevos canales
+@socketio.on('N_Channels')
+def Channel(Canales):
+    # Global Channels
+    global Channels
+    print('CANALESSSSSSSSSSSSSS', Canales['canal'])
+    if Canales['canal'] in Channels:
+        emit('New_Canal', {'codigo': 'existe', 'user': Canales['usuario']}, broadcast=True)
+    else:
+        Channels.append(Canales['canal'])
+        print(Channels)
+        join_room(Canales['canal'])
+        emit('New_Canal', {'Canal':Canales['canal']}, broadcast=True)
+        
+# Unirse al Canal
+@socketio.on('join')
+def on_join(msg):
+    room = msg['canal']
+    print('has entered the room.', room)
+    join_room(room)
+    emit('entrar', {'DS': msg['usuario'] + ' has entered the room.'}, room=room)
+
+# Salir del Canal
+@socketio.on('leave')
+def on_leave(msg):
+    room = msg['canal']
+    print('has leave the room.', room)
+    leave_room(room)
+    emit('salir', {'DS1': msg['usuario'] + ' has leave the room.'}, room=room)
+
+@socketio.on('lista_M')
+def lista(msg):
+    emit('lista',{'lista_m':list(lista_mensaje[msg['canal']])}, room=msg['canal'])
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,113 @@
+   // Conexion a websocket
+let socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+var name = localStorage.getItem('Usuario');
+let canales = localStorage.getItem('Canal');
+
+function limpiarLocalStorage(){
+    localStorage.clear();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // Codigo para enviar mensaje mendiante el teclado ENTER
+    document.querySelector('#Mensaje').addEventListener("keydown", event => {
+        if (event.key == "Enter") {
+            document.getElementById('enviar').click();
+        }
+    });
+    // Boton para enviar el mensaje
+    $('#enviar').on('click', function (){
+        let mensaje = document.querySelector('#Mensaje').value;
+        socket.emit('message', {'usuario': name, 'mensaje': mensaje, 'canal':localStorage.getItem('Canal')});
+        document.getElementById("Mensaje").value = "";
+    });
+
+    // Mensaje a mostrar en la pagina
+    socket.on('messages', function(msg){
+        if(msg.user == name){
+            $('#BoxMessage1').append('<li><div class="messageBox" style="--user-color: #234A65"><span class="meta"><span class="badges"></span><span class="name">'+ msg.user + ' ' +msg.tiempo +'</span><i class="metaBG"></i></span><span class="message" id="texto-msj">'+ msg.msgs + '</span></div></li>' + '<br><br>');
+        }else{
+            $('#BoxMessage1').append('<li><div class="messageBox" style="--user-color: #F4A651"><span class="meta"><span class="badges"></span><span class="name">'+ msg.user + ' ' +msg.tiempo +'</span><i class="metaBG"></i></span><span class="message" id="texto-msj">'+ msg.msgs + '</span></div></li>' + '<br><br>');
+        }
+    });
+
+    // Validar si el canal ya se encuentra creado
+    document.querySelector('#Channel_NEW').onclick = () => {
+
+        let canales_new = document.querySelector('#new_channels').value;
+        let canales_new2 = canales_new.trim()
+        
+        if(canales === canales_new2){
+            alert('Este Canal es incorrecto')
+            console.log('error:')
+        }
+        else{
+            localStorage.setItem('Canal', canales_new2);
+            socket.emit('N_Channels', {'canal': canales_new2, 'usuario': name});
+            console.log('Canal creado:')
+            $('#exampleModal').modal('hide')
+        }
+    }
+    socket.on('New_Canal', function(data) {
+        if (data.codigo === 'existe')
+        {
+            alert("Este canal ya existe");
+        }
+        else
+        {
+            $('#listaCanal').append(`<a onclick="selectCanal('` +data.Canal+ `');" class="nav-link active selectCanal" >`+ data.Canal +`</a>`);
+        } 
+        console.log("BUENA BUENA")
+    });
+    socket.on('entrar', function(data){
+        console.log(data.DS);
+        socket.emit('lista_M',{'canal':localStorage.getItem('Canal')});
+    });
+    socket.on('salir', function(data){
+        console.log(data.DS1);
+    });
+    socket.on('lista', function(data){
+        let room = document.querySelector('#Room');
+        room.value = "";
+        room.innerHTML= `Room: ${localStorage.getItem('Canal')}`;
+        document.querySelector('#BoxMessage1').innerHTML = '';
+        data.lista_m.forEach(element => {
+            if(element.user == name){
+                $('#BoxMessage1').append('<li><div class="messageBox" style="--user-color: #234A65"><span class="meta"><span class="badges"></span><span class="name">'+ element.user + ' ' +element.tiempo +'</span><i class="metaBG"></i></span><span class="message" id="texto-msj">'+ element.msgs + '</span></div></li>' + '<br><br>');
+            }else{
+                $('#BoxMessage1').append('<li><div class="messageBox" style="--user-color: #F4A651"><span class="meta"><span class="badges"></span><span class="name">'+ element.user + ' ' +element.tiempo +'</span><i class="metaBG"></i></span><span class="message"id="texto-msj">'+ element.msgs + '</span></div></li>' + '<br><br>');
+            }
+            
+        });
+    });
+});
+// Funcion para unirse o salir de los canales creados
+function selectCanal(data){
+    if(data == localStorage.getItem('Canal')){
+        console.log('Ya entre a la room:')
+    }else{
+        if(localStorage.getItem('Canal') != null){
+            // Limpiamos los mensaje creado en el canal
+            document.querySelector('#BoxMessage1').innerHTML = '';
+            socket.emit('leave',{'usuario': name, 'canal': localStorage.getItem('Canal')});
+        }
+        socket.emit('join',{'usuario': name, 'canal': data});
+        localStorage.setItem('Canal', data);
+    }
+    let room = document.querySelector('#Room');
+    room.value = "";
+    room.innerHTML= `Room: ${data}`;
+}
+// Validacion para el usuario para unirse al canal predeterminado o al ultimo canal entrado
+if (!localStorage.getItem('Canal')){
+    localStorage.setItem('Canal', 'General');
+    socket.emit('join', {'usuario': name, 'canal':localStorage.getItem('Canal')})
+}else{
+    socket.emit('join', {'usuario': name, 'canal':localStorage.getItem('Canal')})
+}
+// TOQUE PERSONAL Button de emoji FASE DE PRUEBA
+$(document).ready(function() {
+    $('#Mensaje').emojioneArea({
+        pickerPosition:"bottom"
+    });
+})
